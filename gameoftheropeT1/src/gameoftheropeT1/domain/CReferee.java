832783a5b032d15c0bf5c02d83f592a6a9420a8e
gabriel <gabriel.vieira@ua.pt>
2016-03-04 16:00:38 +0000

    Contestants and Referee classes: constructor done and variables initialized

--- gameoftheropeT1/src/gameoftheropeT1/domain/CReferee.java
+++ gameoftheropeT1/src/gameoftheropeT1/domain/CReferee.java
@@ -3,12 +3,45 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package gameoftheropeT1.domain;
+package gameoftheropeT1.domain; 
 
 /**
  *
  * @author roliveira
  */
+import gameoftheropeT1.interfaces.*;
+import newpackage.state.ERefereeState;
+
 public class CReferee extends Thread{
+    private final IRefereeBench bench;
+    private final IRefereeSite site;
+    private final IRefereePlayground playground;
+    private final IRefereeRepository repository;
+    private ERefereeState state;
     
+    
+    public CReferee(IRefereeBench bench, IRefereePlayground playground, IRefereeSite site, IRefereeRepository repository)
+    {
+        this.bench = bench;
+        this.playground = playground;
+        this.site = site;
+        this.repository = repository;
+        
+        state = ERefereeState.START_OF_A_GAME;
+    }
+    
+    @Override
+    public void run() // ciclo de vida do arbitro
+    {
+        
+    }
+            // actualiza estado
+    public void setState(ERefereeState state) {
+        this.state = state;
+    }
+    
+            // retorna estado actual
+    public ERefereeState getCurrentState() {
+        return state;
+    }
 }
--- gameoftheropeT1/src/gameoftheropeT1/sharing/CSite.java
+++ gameoftheropeT1/src/gameoftheropeT1/sharing/CSite.java
@@ -27,4 +27,5 @@ public class CSite implements ICoachSite, IRefereeSite, IContestantsSite{
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
+    
 }
