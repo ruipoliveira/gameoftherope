a262b33d4eb3ecc0ed7ec50ea31d09a82c95b1cc
Rui Oliveira <ruipedrooliveira@ua.pt>
2016-03-05 15:01:37 +0000

    change type constants

--- gameoftheropeT1/src/gameoftheropeT1/main/CConstant.java
+++ gameoftheropeT1/src/gameoftheropeT1/main/CConstant.java
@@ -10,9 +10,9 @@ package gameoftheropeT1.main;
  * @author roliveira
  */
 public class CConstant {
-    public static int GAMES_PER_MATCH = 3; 
-    public static char GAME_CONTINUATION = 'C'; 
-    public static int CONTESTANTS_IN_TRAIL = 3; 
-    public static int MAX_TRAILS_PER_GAME = 6; 
-    public static int KNOCK_OUT = 4;  
+    public final static int GAMES_PER_MATCH = 3; 
+    public final static char GAME_CONTINUATION = 'C'; 
+    public final static int CONTESTANTS_IN_TRAIL = 3; 
+    public final static int MAX_TRAILS_PER_GAME = 6; 
+    public final static int KNOCK_OUT = 4;  
 }
