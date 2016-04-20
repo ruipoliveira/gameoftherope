bbfd6696d388ae11cac89d31a7cc9748f85c9383
gabriel <gabriel.vieira@ua.pt>
2016-04-20 19:42:43 +0100

    construtors for all types of messages... Just 4 implemented

--- gameoftheropeT2/src/Communication/Message/Message.java
+++ gameoftheropeT2/src/Communication/Message/Message.java
@@ -57,6 +57,7 @@ public class Message {
     private String filename;
     
     
+    
     // duvida aqui.... achas que vale a pena ter 2 variaveis para aquele de uma ser no inicio e outra 
     // ser depois quando se desenrola o jogo???
     /**
@@ -93,6 +94,97 @@ public class Message {
      * Variable that holds if the last pulled the rope, in case the message requires it.
      */
     private boolean lastPulled;
+    
+     /******************
+     ** CONSTRUCTORS **
+     ******************/
+    
+    /**
+     * Empty constructor for the message that initializes the default values for the previous variables.
+     */
+    private Message(){
+        id = ERROR_INT;
+        
+        refState = null;
+        coachState = null;
+        contestState = null;
+  
+
+        filename = null;
+
+        contestStrength = ERROR_INT;
+        pullPosition = ERROR_INT;
+        trialNumber = ERROR_INT;
+        gameNumber = ERROR_INT;
+
+        isKnockOut = false;
+        lastPulled = false;
+
+        totalPlayer = ERROR_INT;
+    }
+    
+        // constructor used in all methods
+    public Message(MessageType type) {
+        this();
+        this.type = type;
+    }
+     
+     /** *  @param type Message type for the created message.
+     *
+     * @param value Possible value for the message.
+     */
+    /*public Message(MessageType type, int value) {
+        this();
+        this.type = type;
+        switch(type)
+        {
+            case ENTER_SHOP:
+            case EXIT_SHOP:
+            case GO_SHOPPING:
+            case TRY_AGAIN_LATER:
+            case PRIME_MATERIALS_NEEDED:
+            case BACK_TO_WORK:
+            case COLLECTING_MATERIALS:
+            case PREPARE_TO_PRODUCE:
+            case BATCH_READY_FOR_TRANSFER:
+            case GO_TO_STORE:
+            case SAY_GOODBYE_TO_CUSTOMER:
+                this.id = value;
+                break;
+                
+            case REPLENISH_STOCK:
+                this.nMaterials = value;
+                break;
+            case ACK:
+            case RETURN_TO_SHOP:
+            case GO_TO_WORKSHOP:
+                this.nProducts = value;
+                break;
+            default:
+                System.err.println(type + ", wrong message type!");
+                this.type = MessageType.ERROR;
+                break;
+        }
+    } */
+    
+    // mensagem com o tipo e o estado de cada cliente / entidade
+    public Message(MessageType type, ERefereeState refState) {
+        this();
+        this.type = type;
+        this.refState = refState;
+    }
+    
+    public Message(MessageType type, ECoachesState coachState) {
+        this();
+        this.type = type;
+        this.coachState = coachState;
+    }
+    
+    public Message(MessageType type, EContestantsState contestState) {
+        this();
+        this.type = type;
+        this.contestState = contestState;
+    }
     
     
     
